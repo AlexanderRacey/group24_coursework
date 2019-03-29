1999f3620c0b034c71f7ae922ae4f1ef552af631
Jonathan Binns <40311703@live.napier.ac.uk>
2019-03-29 11:12:49 +0000

    Updated: connect and main

--- src/main/java/com/napier/sem/App.java
+++ src/main/java/com/napier/sem/App.java
@@ -13,7 +13,8 @@ public class App
         App app = new App();
 
         // Connect to the database
-        app.connect();
+        //app.connect();
+        app.connect("localhost:33060");
 
         /**
          * Lists all capital cities in the world by population
@@ -66,52 +67,52 @@ public class App
         /**
          * 10 - list all cities in region by pop
          **/
-
+        /*
         ListAllCities listAllCitiesRegion = new ListAllCities();
         listAllCitiesRegion.citiesInRegion("North America", app.connection);
-
+        */
         /**
          * 11 - list all cities in district by pop
          **/
-
+        /*
         ListAllCities listAllCitiesDistrict = new ListAllCities();
         listAllCitiesDistrict.citiesInDistrict("Scotland",app.connection);
-
+        */
         /**
          * list n cities world, user enters n, by pop
          **/
-
+        /*
         ListAllCities listNCitiesWorld = new ListAllCities();
         listNCitiesWorld.nCitiesInWorld("5",app.connection);
-
+        */
         /**
          * list n cities continent, user enters n & continent, by pop
          **/
-
+        /*
         ListAllCities listNCitiesContinent = new ListAllCities();
         listNCitiesContinent.nCitiesContinent("Asia","5",app.connection);
-
+        */
         /**
          * list n cities region, user enters n & region, by pop
          **/
-
+        /*
         ListAllCities listNCitiesRegion = new ListAllCities();
         listNCitiesRegion.nCitiesInRegion("North America","5",app.connection);
-
+        */
         /**
          * list n cities country, user enters n & country, by pop
          **/
-
+        /*
         ListAllCities listNCitiesCountry = new ListAllCities();
         listNCitiesCountry.nCitiesInCountry("France","5",app.connection);
-
+        */
         /**
          * list n cities district, user enters n & district, by pop
          **/
-
+        /*
         ListAllCities listNCitiesDistrict = new ListAllCities();
         listNCitiesDistrict.nCitiesInDistrict("England","5",app.connection);
-
+        */
 
         // Disconnect from the database
         app.disconnect();
@@ -123,8 +124,39 @@ public class App
     private Connection connection = null;
 
     /**
-     * Connect to the MySQL database.
+     * New Connect to the MySql database
      */
+    public void connect(String location) {
+        try {
+            // Load Database driver
+            Class.forName("com.mysql.cj.jdbc.Driver");
+        } catch (ClassNotFoundException e) {
+            System.out.println("Could not load SQL driver");
+            System.exit(-1);
+        }
+
+        int retries = 10;
+        for (int i = 0; i < retries; ++i) {
+            System.out.println("Connecting to database...");
+            try {
+                // Wait a bit for db to start
+                Thread.sleep(30000);
+                // Connect to database
+                connection = DriverManager.getConnection("jdbc:mysql://" + location + "/employees?allowPublicKeyRetrieval=true&useSSL=false", "root", "example");
+                System.out.println("Successfully connected");
+                break;
+            } catch (SQLException sqle) {
+                System.out.println("Failed to connect to database attempt " + Integer.toString(i));
+                System.out.println(sqle.getMessage());
+            } catch (InterruptedException ie) {
+                System.out.println("Thread interrupted? Should not happen.");
+            }
+        }
+    }
+    /**
+     * Old Connect to the MySQL database.
+     */
+    /*
     public void connect()
     {
         try
@@ -162,7 +194,7 @@ public class App
             }
         }
     }
-
+    */
     /**
      * Disconnect from the MySQL database.
      */
