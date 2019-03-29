de41c36f8aee58bc92b0577ddefe19f499b9f323
Jonathan Binns <40311703@live.napier.ac.uk>
2019-03-29 13:19:59 +0000

    Added: all integration tests

--- src/test/java/com/napier/sem/AppIntegrationTest.java
+++ src/test/java/com/napier/sem/AppIntegrationTest.java
@@ -26,4 +26,88 @@ public class AppIntegrationTest
         ListAllCapitalCities listAllCapitalCities = new ListAllCapitalCities();
         listAllCapitalCities.inTheWorld(null);
     }
+    @Test
+    void listAllCapitalCitiesOnContinentNull()
+    {
+        ListAllCapitalCities listAllCapitalCities = new ListAllCapitalCities();
+        listAllCapitalCities.onContinent(null, null);
+    }
+
+    @Test
+    void listAllCapitalCitiesInRegionNull()
+    {
+        ListAllCapitalCities listAllCapitalCities = new ListAllCapitalCities();
+        listAllCapitalCities.inRegion( null, null);
+    }
+
+    @Test
+    void listAllCitiesInWorldNull()
+    {
+        ListAllCities listAllCitiesWorld = new ListAllCities();
+        listAllCitiesWorld.citiesInWorld(null);
+    }
+
+    @Test
+    void listAllCitiesInContinentNull()
+    {
+        ListAllCities listAllCitiesContinent = new ListAllCities();
+        listAllCitiesContinent.citiesContinent(null, null);
+    }
+
+    @Test
+    void listAllCitiesInCountryNull()
+    {
+        ListAllCities listAllCitiesCountry = new ListAllCities();
+        listAllCitiesCountry.citiesInCountry(null, null);
+    }
+
+    @Test
+    void listAllCitiesInRegionNull()
+    {
+        ListAllCities listAllCitiesRegion = new ListAllCities();
+        listAllCitiesRegion.citiesInRegion(null, null);
+    }
+
+    @Test
+    void listAllCitiesInDistrictNull()
+    {
+        ListAllCities listAllCitiesDistrict = new ListAllCities();
+        listAllCitiesDistrict.citiesInDistrict(null,null);
+    }
+
+    @Test
+    void listAllNCitiesInWorldNull()
+    {
+        ListAllCities listNCitiesWorld = new ListAllCities();
+        listNCitiesWorld.nCitiesInWorld(null,null);
+    }
+
+    @Test
+    void listNCitiesInContinentNull()
+    {
+        ListAllCities listNCitiesContinent = new ListAllCities();
+        listNCitiesContinent.nCitiesContinent(null,null,null);
+    }
+
+    @Test
+    void listNCitiesInRegionNull()
+    {
+        ListAllCities listNCitiesRegion = new ListAllCities();
+        listNCitiesRegion.nCitiesInRegion(null,null,null);
+    }
+
+    @Test
+    void listNCitiesInCountryNull()
+    {
+        ListAllCities listNCitiesCountry = new ListAllCities();
+        listNCitiesCountry.nCitiesInCountry(null,null,null);
+    }
+
+    @Test
+    void listNCitiesInDistrictNull()
+    {
+        ListAllCities listNCitiesDistrict = new ListAllCities();
+        listNCitiesDistrict.nCitiesInDistrict(null,null,null);
+    }
+
 }
