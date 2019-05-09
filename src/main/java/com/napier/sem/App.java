f15fedb642a0f556383c331a0c19ee1fef0d5686
Jonathan Binns <40311703@live.napier.ac.uk>
2019-05-09 17:15:01 +0100

    Updated: re-ordered app.java

--- src/main/java/com/napier/sem/App.java
+++ src/main/java/com/napier/sem/App.java
@@ -21,98 +21,98 @@ public class App
             app.connect(args[0]);
         }
 
-         //Lists all capital cities in the world by population
-         ListAllCapitalCities.inTheWorld(app.connection);
+        //1 - All the countries in the world organised by largest population to smallest
+        ListAllCountries.inTheWorld(app.connection);
 
-         //Lists all Capital Cities in ASIA by population
-         ListAllCapitalCities.onContinent( "Asia", app.connection);
+        //2 - All the countries in a continent organised by largest population to smallest
+        ListAllCountries.onContinent("Asia", app.connection);
 
-         //Lists all Capital Cities in Caribbean by population
-         ListAllCapitalCities.inRegion( "Middle East", app.connection);
+        //3 - All the countries in a region organised by largest population to smallest
+        ListAllCountries.inRegion("North America", app.connection);
 
-         //Lists all N capital cities in the world by population
-         ListAllCapitalCities.nInTheWorld("5",app.connection);
+        //4 - The top N populated countries in the world where N is provided by the user
+        TopCountries.nInTheWorld("5", app.connection);
 
-         //Lists all N Capital Cities in ASIA by population
-         ListAllCapitalCities.nOnContinent( "Asia", "5",app.connection);
+        //5 - The top N populated countries in a continent where N is provided by the user
+        TopCountries.nOnContinent("Asia", "5", app.connection);
 
-         //Lists all N Capital Cities in Caribbean by population
-         ListAllCapitalCities.nInRegion( "Middle East","5", app.connection);
+        //6 - The top N populated countries in a region where N is provided by the user
+        TopCountries.nInRegion("Middle East", "5", app.connection);
 
-         //7 - list all cities in world by pop
-         ListAllCities.citiesInWorld(app.connection);
+        //7 - All the cities in the world organised by largest population to smallest
+        ListAllCities.citiesInWorld(app.connection);
 
-         //8 - list all cities in continent by pop
-         ListAllCities.citiesContinent("Asia", app.connection);
+        //8 - All the cities in a continent organised by largest population to smallest
+        ListAllCities.citiesContinent("Asia", app.connection);
 
-         //9 - list all cities in country by pop
-         ListAllCities.citiesInCountry("France", app.connection);
+        //9 - All the cities in a region organised by largest population to smallest
+        ListAllCities.citiesInCountry("France", app.connection);
 
-         //10 - list all cities in region by pop
-         ListAllCities.citiesInRegion("Middle East", app.connection);
+        //10 - All the cities in a country organised by largest population to smallest
+        ListAllCities.citiesInRegion("Middle East", app.connection);
 
-         //11 - list all cities in district by pop
-         ListAllCities.citiesInDistrict("Scotland",app.connection);
+        //11 - All the cities in a district organised by largest population to smallest
+        ListAllCities.citiesInDistrict("Scotland",app.connection);
 
-         //list n cities world, user enters n, by pop
-         ListAllCities.nCitiesInWorld("5",app.connection);
+        //12 - The top N populated cities in the world where N is provided by the user
+        ListAllCities.nCitiesInWorld("5",app.connection);
 
-         //list n cities continent, user enters n & continent, by pop
-         ListAllCities.nCitiesContinent("Asia","5",app.connection);
+        //13 - The top N populated cities in a continent where N is provided by the user
+        ListAllCities.nCitiesContinent("Asia","5",app.connection);
 
-         //list n cities region, user enters n & region, by pop
-         ListAllCities.nCitiesInRegion("Middle East","5",app.connection);
+        //14 - The top N populated cities in a region where N is provided by the user
+        ListAllCities.nCitiesInRegion("Middle East","5",app.connection);
 
-         //list n cities country, user enters n & country, by pop
-         ListAllCities.nCitiesInCountry("France","5",app.connection);
+        //15 - The top N populated cities in a country where N is provided by the user
+        ListAllCities.nCitiesInCountry("France","5",app.connection);
 
-         //list n cities district, user enters n & district, by pop
-         ListAllCities.nCitiesInDistrict("England","5",app.connection);
+        //16 - The top N populated cities in a district where N is provided by the user
+        ListAllCities.nCitiesInDistrict("England","5",app.connection);
 
-         //Population of the world
-         Extras.worldPop(app.connection);
+        //17 - All the capital cities in the world organised by largest population to smallest
+        ListAllCapitalCities.inTheWorld(app.connection);
 
-         //Population of continent
-         Extras.contPop("Asia", app.connection);
+        //18 - All the capital cities in a continent organised by largest population to smallest
+        ListAllCapitalCities.onContinent( "Asia", app.connection);
 
-         //Population of Region
-         Extras.regionPop("Middle East", app.connection);
+        //19 - All the capital cities in a region organised by largest to smallest
+        ListAllCapitalCities.inRegion( "Middle East", app.connection);
 
-         //Population of Country
-         Extras.countryPop("France", app.connection);
+        //20 - The top N populated capital cities in the world where N is provided by the user
+        ListAllCapitalCities.nInTheWorld("5",app.connection);
 
-         //Population of District
-         Extras.districtPop("England", app.connection);
+        //21 - The top N populated capital cities in a continent where N is provided by the user
+        ListAllCapitalCities.nOnContinent( "Asia", "5",app.connection);
 
-         //Population of City
-         Extras.cityPop("London", app.connection);
+        //22 - The top N populated capital cities in a region where N is provided by the user.
+        ListAllCapitalCities.nInRegion( "Middle East","5", app.connection);
 
-         //list all countries in world
-         ListAllCountries.inTheWorld(app.connection);
+        //23 - The population of people, people living in cities, and people not living in cities in each continent
+        PopulationCitiesInOut.inEachContinent(app.connection);
 
-         //list all countries on continent
-         ListAllCountries.onContinent("Asia", app.connection);
+        //24 - The population of people, people living in cities, and people not living in cities in each region
+        PopulationCitiesInOut.inEachRegion(app.connection);
 
-         //list all countries in region
-         ListAllCountries.inRegion("North America", app.connection);
+        //25 - The population of people, people living in cities, and people not living in cities in each country.
+        PopulationCitiesInOut.inEachCountry(app.connection);
 
-         //list top n countries in world
-         TopCountries.nInTheWorld("5", app.connection);
+        //26 - The population of the world
+        Extras.worldPop(app.connection);
 
-         //list top n countries on continent
-         TopCountries.nOnContinent("Asia", "5", app.connection);
+        //27 - The population of a continent.
+        Extras.contPop("Asia", app.connection);
 
-         //list top n countries in region
-         TopCountries.nInRegion("Middle East", "5", app.connection);
+        //28 - The population of a region
+        Extras.regionPop("Middle East", app.connection);
 
-         //List the population of people, those living in cities and those not living in cities in each continent
-         PopulationCitiesInOut.inEachContinent(app.connection);
+        //29 - The population of a country
+        Extras.countryPop("France", app.connection);
 
-         //List the population of people, those living in cities and those not living in cities in each region
-         PopulationCitiesInOut.inEachRegion(app.connection);
+        //30 - The population of a district
+        Extras.districtPop("England", app.connection);
 
-         //List the population of people, those living in cities and those not living in cities in each country
-         PopulationCitiesInOut.inEachCountry(app.connection);
+        //31 - The population of a city
+        Extras.cityPop("London", app.connection);
 
          // Disconnect from the database
          app.disconnect();
